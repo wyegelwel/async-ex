1e3fbe49df55bf7bb4fa66ac07448637b8243e37
Wil Yegelwel <wyegelwel@gmail.com>
2014-09-27 21:30:19 -0400

    Can use multiple ex-chs with thread-ex and go-ex

--- src/async_ex/core.clj
+++ src/async_ex/core.clj
@@ -10,17 +10,30 @@
        (handler (<! c))))
    c))
 
+(defn coerce-seq 
+  "Takes either a seq or single element and returns a seq"
+  [x]
+  (flatten [x]))
 
-(defmacro go-ex [ex-ch & body]
+(defmacro go-ex [ex-chs & body]
   `(go
      (try 
        ~@body
-       (catch Exception ex#
-         (>! ~ex-ch ex#)))))
+       (catch Exception ~'ex
+         ~@(map (fn [c] (list '>!! c 'ex)) (coerce-seq ex-chs))))))
 
-(defmacro thread-ex [ex-ch & body]
+(defmacro thread-ex [ex-chs & body]
   `(thread
      (try 
        ~@body
-       (catch Exception ex#
-         (>!! ~ex-ch ex#)))))
+       (catch Exception ~'ex
+         ~@(map (fn [c] (list '>!! c 'ex)) (coerce-seq ex-chs))))))
+
+#_ (def erno (atom ""))
+#_ (def erno1 (atom ""))
+#_ (def ex-ch (ex-chan (fn [e] (reset! erno e))))
+#_ (def ex-ch1 (ex-chan (fn [e] (reset! erno1 e))))
+
+#_ (macroexpand '(go-ex 
+     [ex-ch ex-ch1]
+     (throw (Exception.))))
--- test/async_ex/core_test.clj
+++ test/async_ex/core_test.clj
@@ -13,36 +13,52 @@
 
 (deftest go-ex-test
   (testing "go-ex"
-    (let [erno (atom "b")
+    (let [erno1 (atom nil)
+          erno2 (atom nil)
           ex (Exception. "Test Exception") 
-          ex-ch (ex-chan (fn [e] (reset! erno e))) 
+          ex-ch1 (ex-chan (fn [e] (reset! erno1 e)))
+          ex-ch2 (ex-chan (fn [e] (reset! erno2 e)))
           ]
-      (go-ex ex-ch
+      (go-ex ex-ch1
              (throw ex))
       (<!! (timeout 1000)) ; Give time for change to propagate
-      (is (= ex @erno))
+      (is (= ex @erno1))
 
-      (reset! erno true)
+      (reset! erno1 true)
       
-      (go-ex ex-ch
+      (go-ex ex-ch1
              (identity "a"))
       (<!! (timeout 1000))
-      (is @erno))))
+      (is @erno1)
+    
+      (go-ex 
+        [ex-ch1 ex-ch2]
+        (throw ex))
+      (<!! (timeout 1000))
+      (is (= @erno1 @erno2 ex)))))
 
 (deftest thread-ex-test
   (testing "thread-ex"
-    (let [erno (atom "b")
+    (let [erno1 (atom nil)
+          erno2 (atom nil)
           ex (Exception. "Test Exception") 
-          ex-ch (ex-chan (fn [e] (reset! erno e))) 
+          ex-ch1 (ex-chan (fn [e] (reset! erno1 e)))
+          ex-ch2 (ex-chan (fn [e] (reset! erno2 e)))
           ]
-      (thread-ex ex-ch
+      (thread-ex ex-ch1
              (throw ex))
       (<!! (timeout 1000)) ; Give time for change to propagate
-      (is (= ex @erno))
+      (is (= ex @erno1))
 
-      (reset! erno true)
-      
-      (thread-ex ex-ch
+      (reset! erno1 true)
+
+      (thread-ex ex-ch1
              (identity "a"))
       (<!! (timeout 1000))
-      (is @erno))))
+      (is @erno1)
+
+      (thread-ex 
+        [ex-ch1 ex-ch2]
+        (throw ex))
+      (<!! (timeout 1000))
+      (is (= @erno1 @erno2 ex)))))
